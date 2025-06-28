cf8bfc9202dbf017e39eb5e671cd4641b541fc4c
basitS14 <abasitsolkar@gmail.com>
2025-06-28 19:18:37 +0530

    production_structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+from typing import List
+from setuptools import find_packages , setup
+
+HYPHEN_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str] :
+    """
+        This function returns list of requirements from a given path
+    """
+    requirements = []
+
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n" , "") for req in requirements]
+
+        if HYPHEN_DOT in requirements:
+            requirements.remove(HYPHEN_DOT)
+    
+    return requirements
+
+setup(
+    author="basit",
+    version="0.1",
+    name="Loan Approval",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
