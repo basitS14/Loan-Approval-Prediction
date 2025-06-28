78aef6f22aaa82b505b958dab40f6f5a57e29222
basitS14 <abasitsolkar@gmail.com>
2025-06-28 19:45:40 +0530

    add:logging

--- loan_approval/logger/__init__.py
+++ loan_approval/logger/__init__.py
@@ -1,20 +1,16 @@
 import logging
 import os
-
-from from_root import from_root
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-log_dir = 'logs'
-
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
-
-os.makedirs(log_dir, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 
 logging.basicConfig(
-    filename=logs_path,
-    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG,
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO   
 )
