25b191e191614a38380aa41ee31568ff6f06e4ec
basitS14 <abasitsolkar@gmail.com>
2025-07-01 00:29:40 +0530

    fix:bugs

--- loan_approval/utils/main_utils.py
+++ loan_approval/utils/main_utils.py
@@ -25,7 +25,7 @@ def clean_data(df:DataFrame) -> DataFrame:
         df['Gender'] = df["Gender"].fillna(df['Gender'].mode()[0])
         df['Married'] = df["Married"].fillna(df['Married'].mode()[0])
         df['Dependents'] = df["Dependents"].fillna(df['Dependents'].mode()[0])
-        df['Self_Employed'] = df["Self_Employed"].fillna("Unknown")
+        df['Self_Employed'] = df["Self_Employed"].fillna(df["Dependents"]).mode()[0]
         df['Loan_Amount_Term'] = df["Loan_Amount_Term"].fillna(df['Loan_Amount_Term'].mode()[0])
         df['Credit_History'] = df["Credit_History"].fillna(df['Credit_History'].mode()[0])
         df['LoanAmount'] = df["LoanAmount"].fillna(df['LoanAmount'].median())
